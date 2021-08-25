42a34f3771ecc1251a258caca9673472326005e2
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-25 23:15:41 +0530

    Jenkins

--- seed.groovy
+++ seed.groovy
@@ -23,3 +23,25 @@ pipelineJob('CI-Pipelines/frontend') {
     }
   }
 }
+
+
+pipelineJob('CI-Pipelines/catalogue') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://chavitisathish@dev.azure.com/chavitisathish/devops-project/_git/catalogue')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
