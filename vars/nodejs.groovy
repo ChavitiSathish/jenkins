ec74f65648ee99c5e63da292b3eab9db52b02008
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-26 17:20:22 +0530

    Jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,25 +1,3 @@
 def call(){
-  pipeline {
-    agent any
-      stages{
 
-        stage('Download Dependencies'){
-          steps{
-            echo "Download Dependencies"
-          }
-        }
-
-        stage('Find Bugs'){
-          steps{
-            echo "Find Bugs"
-          }
-        }
-
-        stage('Test Cases'){
-          steps{
-            echo "Test Cases"
-          }
-        }
-      }
-  }
 }
