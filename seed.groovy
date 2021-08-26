671140854ab88d827e23f9590d260dc235d1317b
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-26 17:22:33 +0530

    Jenkins

--- seed.groovy
+++ seed.groovy
@@ -3,55 +3,25 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-//pipelineJob('CI-Pipelines/frontend') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/frontend')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
 
-/// Running a loop to lkeep the code dry.
-def component = ["cart", "catalogue","user","payment","shipping","frontend"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://chavitisathish@dev.azure.com/chavitisathish/devops-project/_git/'+j)
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
+pipelineJob('CI-Pipelines/cart') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/cart')
           }
         }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
       }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
     }
   }
 }
+
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,3 +1,25 @@
 def call(){
+  pipeline {
+    agent any
+      stages{
 
+        stage('Download Dependencies'){
+          steps{
+            echo "Download Dependencies"
+          }
+        }
+
+        stage('Find Bugs'){
+          steps{
+            echo "Find Bugs"
+          }
+        }
+
+        stage('Test Cases'){
+          steps{
+            echo "Test Cases"
+          }
+        }
+      }
+  }
 }
