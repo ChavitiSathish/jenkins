0e2f09fa88782986923c347a9841065ce11b4225
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-28 13:46:00 +0530

    NodeJs Jenkins Job

--- vars/bugs.groovy
+++ vars/bugs.groovy
@@ -1,4 +1,4 @@
 def check_bugs(String COMPONENT) {
     sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.7.211:9000 -Dsonar.login=46fec8bc4f8d4830274640ba75d38ddd7f0cdf15"
-    //sh "sonar-quality-gate.sh admin123 admin123 172.31.7.211 ${COMPONENT}"
+    sh "sonar-quality-gate.sh admin123 admin123 172.31.7.211 ${COMPONENT}"
 }
