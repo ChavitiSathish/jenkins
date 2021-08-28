1657668d9b6c3d4fed4fa2965cf70a4e05a8167c
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-28 14:15:13 +0530

    NodeJs Jenkins Job

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,16 +2,17 @@ def call(String COMPONENT) {
   pipeline {
     agent any
 
-    environment {
-      SQ_TOKEN = credentials("SQ_TOKEN")
-      SQ_LOGIN = credentials("SQ_LOGIN")
-    }
+    //environment {
+      //SQ_TOKEN = credentials("SQ_TOKEN")
+      //SQ_LOGIN = credentials("SQ_LOGIN")
+    //}
 
     stages {
       stage('Find Bugs') {
         steps {
           script {
-            bugs.check_bugs(COMPONENT, SQ_TOKEN, SQ_LOGIN_USR, SQ_LOGIN_PSW)
+            //bugs.check_bugs(COMPONENT, SQ_TOKEN, SQ_LOGIN_USR, SQ_LOGIN_PSW)
+            bugs.check_bugs(COMPONENT)
           }
         }
       }
