20641e9044b3cd048cad1788bb4fe1118774612d
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-29 00:09:56 +0530

    NodeJs Jenkins Job

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -26,7 +26,11 @@ def call(String COMPONENT) {
          stage('Publish Artifacts') {
            when { expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) } }
            steps {
-             echo "Publish Artifacts"
+             sh """
+            gitTag=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
+            cd static
+            zip -r ../${COMPONENT}-\${gitTag}.zip node_modules server.js
+          """
            }
          }
        }
