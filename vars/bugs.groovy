aca53913be03696537b3705a4137cbc69dd53837
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-28 11:40:34 +0530

    NodeJs Jenkins Job

--- vars/bugs.groovy
+++ vars/bugs.groovy
@@ -1,3 +1,3 @@
 def check_bugs() {
-    print "Checking Bugs"
+    sh "sonar-scanner -Dsonar.projectKey=cart -Dsonar.sources=. -Dsonar.host.url=http://172.31.7.211:9000 -Dsonar.login=46fec8bc4f8d4830274640ba75d38ddd7f0cdf15"
 }
