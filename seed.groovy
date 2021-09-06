231c6c9b18085a0e0d053684f973580235766dc1
Chaviti Sathish <chavitisathish@gmail.com>
2021-09-06 21:08:56 +0530

    NodeJs Jenkins Job

--- seed.groovy
+++ seed.groovy
@@ -77,3 +77,24 @@ pipelineJob('Create Infra') {
     }
   }
 }
+
+pipelineJob('Destroy Infra') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/ChavitiSathish/tf-mutable.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile-destroy')
+      'lightweight'(true)
+    }
+  }
+}
